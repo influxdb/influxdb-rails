e1691042f5cfeabf2d89553c334d4457eb6c95e8
Eduardo Navarro <enavarro@suse.com>
2024-05-28 14:18:30 +0200

    Fix RSpec/NamedSubject RuboCop offenses manually

--- spec/unit/influx_db/rails/configuration_spec.rb
+++ spec/unit/influx_db/rails/configuration_spec.rb
@@ -6,84 +6,84 @@ RSpec.describe InfluxDB::Rails::Configuration do
   end
 
   describe "client configuration" do
-    subject { InfluxDB::Rails.configuration.client }
+    subject(:configuration_client) { InfluxDB::Rails.configuration.client }
 
     describe "#max_retries" do
       it "defaults to 0" do
-        expect(subject.max_retries).to eq(0)
+        expect(configuration_client.max_retries).to eq(0)
       end
 
       it "can be updated" do
         InfluxDB::Rails.configure do |config|
           config.client.max_retries = 5
         end
-        expect(subject.max_retries).to be(5)
-        expect(subject.write_options.max_retries).to be(5)
+        expect(configuration_client.max_retries).to be(5)
+        expect(configuration_client.write_options.max_retries).to be(5)
       end
     end
 
     describe "#open_timeout" do
       it "defaults to 5 seconds" do
-        expect(subject.open_timeout).to be(5)
+        expect(configuration_client.open_timeout).to be(5)
       end
 
       it "can be updated" do
         InfluxDB::Rails.configure do |config|
           config.client.open_timeout = 5
         end
-        expect(subject.open_timeout).to be(5)
+        expect(configuration_client.open_timeout).to be(5)
       end
     end
 
     describe "#write_timeout" do
       it "defaults to 5 seconds" do
-        expect(subject.write_timeout).to be(5)
+        expect(configuration_client.write_timeout).to be(5)
       end
 
       it "can be updated" do
         InfluxDB::Rails.configure do |config|
           config.client.write_timeout = 5
         end
-        expect(subject.write_timeout).to be(5)
+        expect(configuration_client.write_timeout).to be(5)
       end
     end
 
     describe "#read_timeout" do
       it "defaults to 60 seconds" do
-        expect(subject.read_timeout).to be(60)
+        expect(configuration_client.read_timeout).to be(60)
       end
 
       it "can be updated" do
         InfluxDB::Rails.configure do |config|
           config.client.read_timeout = 5
         end
-        expect(subject.read_timeout).to be(5)
+        expect(configuration_client.read_timeout).to be(5)
       end
     end
 
     describe "#precision" do
       it "defaults to milli seconds" do
-        expect(subject.precision).to eql(InfluxDB2::WritePrecision::MILLISECOND)
+        expect(configuration_client.precision).to eql(InfluxDB2::WritePrecision::MILLISECOND)
       end
 
       it "can be updated" do
         InfluxDB::Rails.configure do |config|
           config.client.precision = InfluxDB2::WritePrecision::NANOSECOND
         end
-        expect(subject.precision).to eql(InfluxDB2::WritePrecision::NANOSECOND)
+        expect(configuration_client.precision).to eql(InfluxDB2::WritePrecision::NANOSECOND)
       end
     end
 
     describe "#async" do
       it "set write_type to batching by default" do
-        expect(subject.write_options.write_type).to eql(InfluxDB2::WriteType::BATCHING)
+        expect(configuration_client.write_options.write_type).to eql(InfluxDB2::WriteType::BATCHING)
       end
 
       it "can be updated" do
         InfluxDB::Rails.configure do |config|
           config.client.async = false
         end
-        expect(subject.write_options.write_type).to eql(InfluxDB2::WriteType::SYNCHRONOUS)
+        expect(configuration_client.write_options.write_type).to eql(InfluxDB2::WriteType::SYNCHRONOUS)
       end
     end
   end
